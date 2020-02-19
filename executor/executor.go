3127d61d4c893660b42cbdd407597a7f79d43993
Carlos <cflores@xubio.com>
2020-02-19 12:49:19 -0300

    [feature/cflores-svc130]
    Se completa Executor para poder procesar combinaciones complejas de formulas: Sum(), GetParamValue(), sumExt()

--- executor/executor.go
+++ executor/executor.go
@@ -17,7 +17,7 @@ type Executor struct {
 func NewExecutor(db *gorm.DB) *Executor {
 	var executor *Executor = new(Executor)
 	executor.db = db
-
+	executor.stack = [][]structFunction.Value{}
 	return executor
 }
 
@@ -29,14 +29,16 @@ func (executor *Executor) Diff(val1 int64, val2 int64) int64 {
 	return val1 - val2
 }
 
-func (executor *Executor) GetParam(paramName string) int64 {
+func (executor *Executor) GetParamValue(paramName string) int64 {
 	length := len(executor.stack)
 	var result int64 = 0
-	argsResolved := executor.stack[length-1]
-	for i := 0; i < len(argsResolved); i++ {
-		if argsResolved[i].Name == paramName {
-			result = argsResolved[i].Valuenumber
-			break
+	if length > 0 {
+		argsResolved := executor.stack[length-1]
+		for i := 0; i < len(argsResolved); i++ {
+			if argsResolved[i].Name == paramName {
+				result = argsResolved[i].Valuenumber
+				break
+			}
 		}
 	}
 
@@ -77,11 +79,13 @@ func (executor *Executor) GetValueFromInvoke(invoke *structFunction.Invoke) (*st
 			return valueResult, nil
 		}
 	} else {
+		// Formula de usuario
 
-		err := errors.New("The function is not primitive.")
-		return nil, err
+		if len(invoke.Args) != len(function.Params) {
+			err := errors.New("The number of params is not adapted.")
+			return nil, err
+		}
 
-		//formula de usurio
 		argsResolved := make([]structFunction.Value, len(function.Params))
 		for i := 0; i < len(function.Params); i++ {
 			valueResolved, err := executor.GetValueResolved(&invoke.Args[i])
@@ -95,7 +99,7 @@ func (executor *Executor) GetValueFromInvoke(invoke *structFunction.Invoke) (*st
 
 		// Push on Stack
 		executor.stack = append(executor.stack, argsResolved)
-		result, err := executor.GetValueFromInvoke(invoke)
+		result, err := executor.GetValueFromInvoke(function.Value.Valueinvoke)
 		if err != nil {
 			return nil, err
 		}
@@ -109,7 +113,7 @@ func (executor *Executor) GetValueFromInvoke(invoke *structFunction.Invoke) (*st
 }
 
 func (executor *Executor) call(function structFunction.Function, args []structFunction.Value) (result []reflect.Value, err error) {
-	myClassValue := reflect.ValueOf(&Executor{})
+	myClassValue := reflect.ValueOf(executor)
 	m := myClassValue.MethodByName(function.Name)
 	if !m.IsValid() {
 		return make([]reflect.Value, 0), fmt.Errorf("Method not found \"%s\"", function.Name)
